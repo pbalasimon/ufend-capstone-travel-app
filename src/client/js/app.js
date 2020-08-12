a4efe2b3664adb1a58e2a00d9c1ecba09e2b5600
Pedro Bala Simón <pbalasimon@gmail.com>
2020-08-12 12:04:32 +0200

    feat: Get geonames info from server

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,11 +1,6 @@
 import moment from "moment";
 import { isDateValid } from "./URLValidator";
 
-const GEONAMES_USERNAME = "pbalasimon";
-let geoNamesEP = `http://api.geonames.org/searchJSON?q=#to&maxRows=10&username=${GEONAMES_USERNAME}`;
-let weatherbitEP = `https://api.weatherbit.io/v2.0/#moment?city=#city,#countryCode&key=e5040dec0dd7436fb983c67d132f654e`;
-let pixabayEP = `https://pixabay.com/api/?key=17854794-ba0c54f1d8e93e506d0d72c4a&q=#cityName+city&image_type=photo&pretty=true`;
-
 async function findTrip(event) {
   event.preventDefault();
 
@@ -43,73 +38,23 @@ async function findTrip(event) {
 
   const info = await getGeonameInfo(to);
   const { name, countryCode, countryName } = info;
-
-  const weather = await getWeather(forecast, name, countryCode);
-  console.log(weather);
-
-  const photo = await getCityPhoto(name);
-  console.log(photo);
-
-  showResults(to, departing, countryName, weather.data[0], photo);
 }
 
-async function getGeonameInfo(to) {
+const getGeonameInfo = (to) => {
   try {
-    const url = geoNamesEP.replace("#to", to);
-    const result = await fetch(url);
-    const data = await result.json();
-    const cityInfo = data.geonames[0];
-    return cityInfo;
+    const info = fetch("http://localhost:8081/cityInfo", {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ to }),
+    });
+    return info;
   } catch (error) {
+    // FIXME
     console.error(error);
   }
-}
+};
 
-async function getWeather(forecast, cityName, countryCode) {
-  try {
-    const url = weatherbitEP
-      .replace("#moment", forecast)
-      .replace("#city", cityName)
-      .replace("#countryCode", countryCode);
-    const result = await fetch(url);
-    const data = await result.json();
-    return data;
-  } catch (error) {
-    console.error;
-  }
-}
-
-async function getCityPhoto(cityName) {
-  try {
-    const url = pixabayEP.replace("#cityName", cityName);
-    const result = await fetch(url);
-    const data = await result.json();
-    return data.hits[0].webformatURL;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-async function showResults(to, departing, countryName, weather, photo) {
-  document.querySelector("#results").classList.remove("hide");
-
-  document.querySelector("#resultToValue").innerText = to + ", " + countryName;
-  document.querySelector("#resultDepartingValue").innerText = departing;
-  const now = moment();
-
-  const momentDeparting = moment(departing, "YYYY-MM-DD");
-  const days = momentDeparting.diff(now, "days");
-  document.querySelector("#resultDaysValue").innerText =
-    to + ", " + countryName + " is " + days + " days away";
-  const temp = weather.temp;
-  const weatherDescription = weather.weather.description;
-  document.querySelector("#weatherValue").innerText =
-    temp + ". " + weatherDescription;
-  document.querySelector("#cityPhotoValue").src = photo;
-}
-
-function printTrip() {
-  window.print();
-}
-
-export { findTrip, printTrip };
+export { findTrip };
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json());
 app.use(
@@ -13,6 +14,29 @@ app.use(
 
 app.use(cors());
 app.use(express.static("dist"));
+
+const GeocoderGeonames = require("geocoder-geonames"),
+  geocoder = new GeocoderGeonames({
+    username: process.env.GEONAMES_USERNAME,
+  });
+
+app.post("/cityInfo", (req, res, next) => {
+  console.log(req.body);
+  console.log(process.env.GEONAMES_USERNAME);
+  geocoder
+    .get("search", {
+      q: req.body.to,
+      maxRows: 10,
+    })
+    .then(function (response) {
+      const cityInfo = response.geonames[0];
+      console.log(cityInfo);
+      res.send(cityInfo);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
 
 app.listen(8081, () => {
   console.log("Travel app listening on port 8081!");
